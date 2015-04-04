fdebdff59faf341da24132c38d8e8d8c6da94875
kmincho <kmincho@hotmail.com>
2015-04-04 22:16:30 +0900

    Tic-Tac-Toe
    completed!!!

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,9 +6,12 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-	static int[][] board = { {0, 0, 0},
-						{0, 0, 0},
-						{0, 0, 0}};
+	static int[][] board = { 	{ 0,  0,  0},
+								{ 0,  0,  0},
+								{ 0,  0,  0}};
+//	static int[][] board = { 	{ 1,  1,  0},
+//								{ -1,  -1,  0},
+//								{ 0,  0,  0}};
 	public static void main(String[] args) {
 		int winner = 0;
 		Scanner scanner = new Scanner(System.in);
@@ -19,6 +22,7 @@ public class TicTacToe {
 		int maxTurn = 9;
 		int curTurn = 0;
 		while(curTurn < maxTurn) {
+			printBoard();
 			String whosTurn = turn ? "A" : "B";
 			System.out.println(whosTurn + "'s turn");
 			input = scanner.nextLine();
@@ -26,10 +30,9 @@ public class TicTacToe {
 			pos[0] = Integer.valueOf(tokens[0]);
 			pos[1] = Integer.valueOf(tokens[1]);
 			board[pos[0]][pos[1]] = turn ? 1 : -1;
-			System.out.println("x = " + pos[0] + ", y = " + pos[1]);
 			turn = !turn;
-			printBoard();
 			if ((winner = checkWinner(pos)) != 0) {
+				printBoard();
 			    if (winner == 1) {
 			        System.out.println("WINNER : A");
 			    } else {
@@ -38,6 +41,7 @@ public class TicTacToe {
 			    break;
 			}
 		}
+		scanner.close();
 		if (winner == 0) {
 			System.out.println("Tie");
 		} else {
@@ -46,38 +50,58 @@ public class TicTacToe {
 	}
 
     public static int checkWinner(int[] pos) {
-        // TO-DO: I need to implement this function
         boolean checkResult = true;
-        if (pos[0] == 0 || pos[0] == 2) {
-            checkResult = true;
-            int k = pos[0];
-            int j;
-            for (j = 0; j < 2; j++) {
-                System.out.println("b1 = " + board[j][k] + ", b2 = " + board[j+1][k]);
-                if (board[j][k] != board[j + 1][k]) {
-                    checkResult = false;
-                    break;
-                }
-            }
-            if (checkResult) {
-                System.out.println("11111 - " + j);
-                return board[pos[0]][pos[1]];
-            }
-        }
-        if (pos[1] == 0 || pos[1] == 2) {
-            checkResult = true;
-            int k = pos[1];
-            for (int j = 0; j < 2; j++) {
-                if (board[k][j] != board[k][j + 1]) {
-                    checkResult = false;
-                    break;
-                }
-            }
-            if (checkResult) {
-                System.out.println("22222");
-                return board[pos[0]][pos[1]];
-            }
-        }
+        int k;
+        
+        //given a column, check every row
+		checkResult = false;
+		k = pos[0];
+		int j;
+		for (j = 0; j < 2; j++) {
+			if (board[k][j] != board[k][j + 1]) {
+				break;
+			}
+		}
+		if (j == 2) {
+			checkResult = true;
+		}
+        
+        //given a row, check every column
+		if (!checkResult) {
+			k = pos[1];
+			for (j = 0; j < 2; j++) {
+				if (board[j][k] != board[j + 1][k]) {
+					break;
+				}
+			}
+			if (j == 2) {
+				checkResult = true;
+			}
+		}
+
+		//check diagonal
+		if (!checkResult) {
+			if (pos[0] == pos[1]) {
+				for (j = 0; j < 2; j++) {
+					if (board[j][j] != board[j + 1][j + 1]) {
+						break;
+					}
+				}
+				
+			} else if (pos[0] + pos[1] == 2) {
+				for (j = 0; j < 2; j++) {
+					if (board[j][2 - j] != board[j + 1][1 - j]) {
+						break;
+					}
+				}
+			}
+			if (j == 2) {
+				checkResult = true;
+			}
+		}
+		if (checkResult) {
+			return board[pos[0]][pos[1]];
+		}
 
         return 0;
     }
