713dac879f617229df77e042452ddf2b2085d765
kmincho <kmincho@hotmail.com>
2015-04-07 06:28:05 +0900

    Collection
    Coding practice with various collections

---
+++ src/javaTest/Collection.java
@@ -0,0 +1,29 @@
+package javaTest;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Collection {
+    public static void main(String[] args) {
+        ArrayListPractice();
+    }
+    
+    public static void ArrayListPractice() {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(1);
+        al.add(3);
+        al.add(2);
+        al.add(4);
+        al.add(8);
+        al.add(7);
+        al.add(6);
+        Iterator<Integer> itr = al.iterator();
+//        while (itr.hasNext()) {
+//            System.out.println(itr.next());
+//        }
+        
+        for (Integer n : al) {
+            System.out.println(n);
+        }
+    }
+}
