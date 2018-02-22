2a754733b76062b56a38aef397c9d11a785a4208
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-02-22 10:49:02 -0800

    BigFactorial

---
+++ src/algorithm/BigFactorial.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        long value = scanner.nextLong();
+
+        BigInteger result = BigInteger.ONE;
+
+        for (long i = 1; i < value + 1; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(result);
+    }
+}
