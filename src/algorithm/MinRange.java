3e6cffb581540465653546725d2796fc74c05a9d
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-02-28 22:36:35 -0800

    MinRange

--- src/algorithm/MinRange.java
+++ src/algorithm/MinRange.java
@@ -1,6 +1,7 @@
 package algorithm;
 
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * https://careercup.com/question?id=16759664
@@ -17,20 +18,97 @@ import java.util.Scanner;
 
 public class MinRange {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int[][] input = {
+                {4, 10, 15, 24, 26},
+                {0, 9, 12, 20},
+                {5, 18, 22, 30}
+        };
 
-        int listCount = scanner.nextInt();
-        scanner.nextLine();
+        int[] index = {0 ,0, 0};
 
-        int[] listSize = new int[listCount];
-        for (int i = 0; i < listCount; i++) {
-            listSize[i] = scanner.nextInt();
+//        int[][] input = {
+//                {24, 4, 10, 15, 26},
+//                {9, 20, 0, 12},
+//                {5, 18, 22, 30},
+//                {7}
+//        };
+//
+//        int[] index = {0 ,0, 0, 0};
+
+        sortArrays(input);
+
+        int minRangeSize = Integer.MAX_VALUE;
+        int[] minRange = {0, 0};
+
+        while (index != null) {
+            int[] range = getRange(input, index);
+            int rangeSize = range[1] - range[0];
+            if (rangeSize < minRangeSize) {
+                minRangeSize = rangeSize;
+                minRange = range;
+            }
+
+            index = moveIndex(input, index);
         }
-        scanner.nextLine();
-        for (int i = 0; i < listCount; i++) {
-            for (int j = 0; j < listSize[i]; j++) {
 
+        System.out.println(Arrays.toString(minRange));
+    }
+
+    static void sortArrays(int[][] input) {
+        for (int i = 0; i < input.length; i++) {
+            Arrays.sort(input[i]);
+        }
+    }
+
+    static int[] getRange(int[][] inputArray, int[] indexArray) {
+        int[] values = getValues(inputArray, indexArray);
+
+        int min = IntStream.of(values).min().getAsInt();
+        int max = IntStream.of(values).max().getAsInt();
+
+        return new int[] {min, max};
+    }
+
+    static int[] moveIndex(int[][] inputArray, int[] indexArray) {
+
+        int index = getArrayIndexToMove(inputArray, indexArray);
+
+        if (index == -1) {
+            return null;
+        }
+
+        indexArray[index]++;
+
+        return indexArray;
+    }
+
+    static int[] getValues(int[][] inputArray, int[] indexArray) {
+        int[] values = new int[inputArray.length];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            values[i] = inputArray[i][indexArray[i]];
+        }
+
+        return values;
+    }
+
+    static int getArrayIndexToMove(int[][] inputArray, int[] indexArray) {
+        int[] values = getValues(inputArray, indexArray);
+
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+
+        for (int i = 0; i < values.length; i++) {
+            if (indexArray[i] == inputArray[i].length - 1) {
+                continue;
+            }
+
+            if (values[i] < min) {
+                index = i;
+                min = values[i];
             }
         }
+
+        return index;
     }
 }
