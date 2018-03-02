248f16d607cb6720528423bd3dd5c28db9d7254f
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-03-02 00:11:12 -0800

    Selection Sort

---
+++ src/com/kmcho/sort/Selection.java
@@ -0,0 +1,24 @@
+package com.kmcho.sort;
+
+import java.util.Arrays;
+
+public class Selection {
+    public static void main(String[] args) {
+        int[] data = {11, 25, 12, 22, 64};
+
+        for (int i = 0; i < data.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int value = data[i];
+            data[i] = data[minIndex];
+            data[minIndex] = value;
+        }
+
+        System.out.println(Arrays.toString(data));
+    }
+}
