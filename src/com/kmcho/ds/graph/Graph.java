aee1ea8f8eb557af405cbf9c36f464055a2b8f52
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-03-31 14:24:09 -0700

    DFS

--- src/com/kmcho/ds/graph/Graph.java
+++ src/com/kmcho/ds/graph/Graph.java
@@ -7,7 +7,26 @@ public class Graph {
     int size;
     int[][] matrix;
 
-    private void BFS(int start) {
+    private void runDFS(int start) {
+        boolean[] visited = new boolean[size];
+        DFS(start, visited);
+        System.out.println();
+    }
+
+    private void DFS(int current, boolean[] visited) {
+        visited[current] = true;
+        System.out.print(current + " ");
+
+        for (int next = 0; next < size; next++) {
+            if (hasEdge(current, next) && !visited[next]) {
+                DFS(next, visited);
+            }
+        }
+
+        return;
+    }
+
+    private void runBFS(int start) {
         boolean[] visited = new boolean[size];
 
         Queue<Integer> queue = new LinkedList<>();
@@ -15,17 +34,18 @@ public class Graph {
         visited[start] = true;
 
         while (queue.size() > 0) {
-            int v = queue.poll();
+            int current = queue.poll();
 
-            System.out.print(v + " ");
+            System.out.print(current + " ");
 
-            for (int j = 0; j < size; j++) {
-                if (existEdge(v, j) && !visited[j]) {
-                    queue.add(j);
-                    visited[j] = true;
+            for (int next = 0; next < size; next++) {
+                if (hasEdge(current, next) && !visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
                 }
             }
         }
+        System.out.println();
     }
 
     public Graph(int size) {
@@ -37,7 +57,7 @@ public class Graph {
         matrix[from][to] = 1;
     }
 
-    public boolean existEdge(int from, int to) {
+    public boolean hasEdge(int from, int to) {
         return matrix[from][to] == 1;
     }
 
@@ -50,26 +70,29 @@ public class Graph {
         graph.addEdge(2, 3);
         graph.addEdge(3, 3);
 
-        graph.BFS(2);
+        graph.runBFS(2);
+        graph.runDFS(2);
 
-//        Graph graph = new Graph(6);
-//        graph.addEdge(0, 1);
-//        graph.addEdge(0, 2);
-//        graph.addEdge(1, 0);
-//        graph.addEdge(1, 3);
-//        graph.addEdge(1, 4);
-//        graph.addEdge(2, 0);
-//        graph.addEdge(2, 4);
-//        graph.addEdge(3, 1);
-//        graph.addEdge(3, 4);
-//        graph.addEdge(3, 5);
-//        graph.addEdge(4, 1);
-//        graph.addEdge(4, 2);
-//        graph.addEdge(4, 3);
-//        graph.addEdge(4, 5);
-//        graph.addEdge(5, 3);
-//        graph.addEdge(5, 4);
-//
-//        graph.BFS(0);
+
+        Graph graph2 = new Graph(6);
+        graph2.addEdge(0, 1);
+        graph2.addEdge(0, 2);
+        graph2.addEdge(1, 0);
+        graph2.addEdge(1, 3);
+        graph2.addEdge(1, 4);
+        graph2.addEdge(2, 0);
+        graph2.addEdge(2, 4);
+        graph2.addEdge(3, 1);
+        graph2.addEdge(3, 4);
+        graph2.addEdge(3, 5);
+        graph2.addEdge(4, 1);
+        graph2.addEdge(4, 2);
+        graph2.addEdge(4, 3);
+        graph2.addEdge(4, 5);
+        graph2.addEdge(5, 3);
+        graph2.addEdge(5, 4);
+
+        graph2.runBFS(0);
+        graph2.runDFS(0);
     }
 }
