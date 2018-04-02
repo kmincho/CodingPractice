26e1ca35aa65616e3e0760faf530e617b06b370a
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-04-02 16:58:51 -0700

    Graph cycle detection

---
+++ src/com/kmcho/gfg/graph/GfG.java
@@ -0,0 +1,42 @@
+package com.kmcho.gfg.graph;
+
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class GfG {
+    public boolean hasCycle(int v, LinkedList<Integer>[] alist, boolean[] visited, boolean[] explored) {
+        for (int i = 0; i < v; i++) {
+            if (DFS(i, alist, visited, explored)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean DFS(int v, LinkedList<Integer>[] alist, boolean[] visited, boolean[] explored) {
+        if (visited[v]) {
+            return false;
+        }
+
+        visited[v] = true;
+        explored[v] = true;
+
+        Iterator<Integer> iterator = alist[v].iterator();
+
+        while (iterator.hasNext()) {
+            int next = iterator.next();
+            if (explored[next]) {
+                return true;
+            } else {
+                if (DFS(next, alist, visited, explored)) {
+                    return true;
+                }
+            }
+        }
+
+        explored[v] = false;
+
+        return false;
+    }
+}
