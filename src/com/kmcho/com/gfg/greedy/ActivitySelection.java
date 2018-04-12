5e1489cdad8e084727fe433d277a1fc83d182d9f
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-04-12 14:23:54 -0700

    Greedy Algorithm - Activity Selection Problem

---
+++ src/com/kmcho/com/gfg/greedy/ActivitySelection.java
@@ -0,0 +1,55 @@
+package com.kmcho.com.gfg.greedy;
+
+import java.util.*;
+
+/**
+ * Greedy Algorithm - Activity Selection Problem
+ * https://practice.geeksforgeeks.org/problems/activity-selection/0
+ */
+
+public class ActivitySelection {
+    static class Task {
+        int start;
+        int end;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int c = scanner.nextInt();
+
+            Task[] tasks = new Task[c];
+
+            for (int j = 0; j < c; j++) {
+                tasks[j] =  new Task();
+            }
+
+            for (int j = 0; j < c; j++) {
+                tasks[j].start = scanner.nextInt();
+            }
+            for (int j = 0; j < c; j++) {
+                tasks[j].end = scanner.nextInt();
+            }
+
+            activitySelection(tasks);
+        }
+    }
+
+    private static void activitySelection(Task[] tasks) {
+        Arrays.sort(tasks, Comparator.comparing((Task task) -> task.end));
+
+        List<Task> output = new ArrayList<>();
+        int last = 0;
+        output.add(tasks[0]);
+
+        for (int i = last + 1; i < tasks.length; i++) {
+            if (tasks[last].end <= tasks[i].start) {
+                output.add(tasks[i]);
+                last = i;
+            }
+        }
+
+        System.out.println(output.size());
+    }
+}
