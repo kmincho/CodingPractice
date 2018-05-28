4f3ee829df12c0530b7f2c913b74305dc9f17427
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-05-28 12:27:10 -0700

    Update Collection

--- src/main/com/kmcho/java/Collection.java
+++ src/main/com/kmcho/java/Collection.java
@@ -1,44 +1,56 @@
 package com.kmcho.java;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Vector;
+import java.util.*;
 
 public class Collection {
     public static void main(String[] args) {
-        ArrayListPractice();
-        System.out.println();
-
-        VectorPractice();
+        arrayListPractice();
+        vectorPractice();
     }
     
-    static void ArrayListPractice() {
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        al.add(1);
-        al.add(3);
-        al.add(2);
-        al.add(4);
-        al.add(8);
-        al.add(7);
-        al.add(6);
-        Iterator<Integer> itr = al.iterator();
-//        while (itr.hasNext()) {
-//            System.out.println(itr.next());
-//        }
-        
-        for (Integer n : al) {
-            System.out.println(n);
-        }
+    private static void arrayListPractice() {
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(1, 3, 2, 4, 8, 7, 6));
+
+        al.add(9);
+
+        printList1(al);
+        printList2(al);
+        printList3(al);
+        printList4(al);
     }
 
-    static void VectorPractice() {
-        Vector v = new Vector();
+    private static void vectorPractice() {
+        Vector v = new Vector(Arrays.asList(1, 2, "Tiger"));
+        v.add(5);
+        v.add("Lion");
 
-        v.add(1);
-        v.add(2);
-        v.add("Tiger");
+        printList3(v);
+        System.out.println("element (idx = 2) in vector = " + v.get(2));
+        System.out.println("vector size = " + v.size());
+        System.out.println("vector capacity = " + v.capacity());
+    }
 
-        System.out.println(v.get(2));
-        System.out.println(v.capacity());
+    private static void printList1(List<Integer> al) {
+        Iterator<Integer> itr = al.iterator();
+        while (itr.hasNext()) {
+            System.out.print(itr.next());
+            if (itr.hasNext()) System.out.print(", ");
+        }
+        System.out.println();
+    }
+
+    private static void printList2(List<Integer> al) {
+        for (Integer n : al) {
+            System.out.print(n + ", ");
+        }
+        System.out.println();
+    }
+
+    private static void printList3(List<Integer> al) {
+        System.out.println(al);
+    }
+
+    private static void printList4(List<Integer> al) {
+        al.forEach(System.out::println);
     }
 }
