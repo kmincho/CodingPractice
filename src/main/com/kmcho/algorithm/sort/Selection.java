ea26e429ad803979f71862c3c3716f63a1641956
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-05-28 01:11:49 -0700

    Update Selection Sort

--- src/main/com/kmcho/algorithm/sort/Selection.java
+++ src/main/com/kmcho/algorithm/sort/Selection.java
@@ -6,19 +6,21 @@ public class Selection {
     public static void main(String[] args) {
         int[] data = {11, 25, 12, 22, 64};
 
-        for (int i = 0; i < data.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < data.length; j++) {
-                if (data[j] < data[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-            int value = data[i];
-            data[i] = data[minIndex];
-            data[minIndex] = value;
-        }
-
+        sort(data);
         System.out.println(Arrays.toString(data));
+    }
+
+    private static void sort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[i] > data[j]) swap(data, i, j);
+            }
+        }
+    }
+
+    private static void swap(int[] data, int i, int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
     }
 }
