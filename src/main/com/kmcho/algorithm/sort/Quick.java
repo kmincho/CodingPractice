9f411572b0514b2088b7c5b4b84ea3def5d84368
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-05-28 08:34:43 -0700

    Update Quick Sort

--- src/main/com/kmcho/algorithm/sort/Quick.java
+++ src/main/com/kmcho/algorithm/sort/Quick.java
@@ -11,37 +11,53 @@ public class Quick {
         System.out.println(Arrays.toString(data));
     }
 
-    static void quickSort(int[] data, int left, int right) {
-        int i;
-        int j;
-        int pivot;
-
-        if (left < right) {
-            i = left;
-            j = right;
-            pivot = data[left];
-
-            while (i < j) {
-                while(data[j] > pivot) {
-                    j--;
-                }
-
-                while(i < j && data[i] <= pivot) {
-                    i++;
-                }
-
-                swap(data, i, j);
-            }
-
-            data[left] = data[i];
-            data[i] = pivot;
-
-            quickSort(data, left, i - 1);
-            quickSort(data, i + 1, right);
+    private static void quickSort(int[] data, int start, int end) {
+        if (start < end) {
+            int m = partition2(data, start, end);
+            quickSort(data, start, m - 1);
+            quickSort(data, m + 1, end);
         }
     }
 
-    static void swap(int[] array, int a, int b) {
+    private static int partition(int[] data, int start, int end) {
+        int i = start;
+        int j = end;
+        int pivot = data[start];
+
+        while (i < j) {
+            while(data[j] > pivot) {
+                j--;
+            }
+
+            while(i < j && data[i] <= pivot) {
+                i++;
+            }
+
+            swap(data, i, j);
+        }
+
+        data[start] = data[i];
+        data[i] = pivot;
+
+        return i;
+    }
+
+    private static int partition2(int[] data, int start, int end) {
+        int pivot = end;
+        int i = start;
+        int j = start;
+
+        while (j < end) {
+            if (data[j] < data[pivot]) {
+                swap(data, i++, j);
+            }
+            j++;
+        }
+        swap(data, i, pivot);
+        return i;
+    }
+
+    private static void swap(int[] array, int a, int b) {
         int t = array[a];
         array[a] = array[b];
         array[b] = t;
