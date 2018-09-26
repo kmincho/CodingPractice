2fcca01bce6810d371051712a3993732689157ea
Kyungmin Cho <kevin.kmincho@gmail.com>
2018-09-26 15:56:43 -0700

    Update TwoSumTest.java

--- src/test/com/kmcho/com/linkedin/TwoSumTest.java
+++ src/test/com/kmcho/com/linkedin/TwoSumTest.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @Config(manifest=Config.NONE)
 @RunWith(RobolectricTestRunner.class)
@@ -18,11 +19,11 @@ public class TwoSumTest {
         twoSum.store(3);
         twoSum.store(6);
 
-        assertEquals(twoSum.test(4), true);
-        assertEquals(twoSum.test(-1), true);
-        assertEquals(twoSum.test(9), true);
-        assertEquals(twoSum.test(10), false);
-        assertEquals(twoSum.test(5), false);
-        assertEquals(twoSum.test(0), false);
+        assertTrue(twoSum.test(4));
+        assertTrue(twoSum.test(-1));
+        assertTrue(twoSum.test(9));
+        assertFalse(twoSum.test(10));
+        assertFalse(twoSum.test(5));
+        assertFalse(twoSum.test(0));
     }
 }
